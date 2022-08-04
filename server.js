cc0d60b01e084317bb6486ad0555a3c8b967c562
abdo-elbeltagy <abdo.elbeltagy401@gmail.com>
2022-08-04 06:33:30 +0200

    adding youtube ref to .ignore

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-// this vedio is from this very good play list ... just have it tell the end
-// https://www.youtube.com/watch?v=JZXQ455OT3A&list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw
-
-
-/////////////////////////////////////////////////////////////////////////////
-
-
-// every thing will be ok ... just be carfull and all will be solved
 console.log('hello world')
 const {add}=require('./math')
 console.log(add(2,3))
+
+// console.log(global)
+
+const os=require('os')
+console.log(os.type())
+console.log(os.version())
+console.log(os.homedir())
