58dc434c680f8135bcc08a3fdbde4d2c163427ce
abdo-elbeltagy <abdo.elbeltagy401@gmail.com>
2022-08-04 06:48:20 +0200

    removing the comment

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const {add,sub}=require('./math')  // we can destructure the functions in the mo
 console.log(add(2,3))
 console.log(sub(2,3))
 
-// console.log(global)
+console.log(global)
 
-// console.log(os.type())
-// console.log(os.version())
-// console.log(os.homedir())
+console.log(os.type())
+console.log(os.version())
+console.log(os.homedir())
 
-// console.log(__dirname) // for directory names
-// console.log(__filename)  // for file names
+console.log(__dirname) // for directory names
+console.log(__filename)  // for file names
 
-// console.log(path.dirname(__filename))
-// console.log(path.basename(__filename))
-// console.log(path.extname(__filename))
-// console.log(path.parse(__filename))
+console.log(path.dirname(__filename))
+console.log(path.basename(__filename))
+console.log(path.extname(__filename))
+console.log(path.parse(__filename))
