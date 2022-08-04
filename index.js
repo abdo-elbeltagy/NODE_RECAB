b66c98c4fc642288501104469f9abde3440f38dd
abdo-elbeltagy <abdo.elbeltagy401@gmail.com>
2022-08-05 01:49:13 +0200

    pakage.json understanding

---
+++ index.js
@@ -0,0 +1,10 @@
+// const os=require('os')
+const path=require('path')
+const fspromises=require('fs').promises
+const {v4:uuid}=require('uuid')
+
+//installing backages... 
+
+console.log('hello. world...')
+const id=uuid()
+console.log(id)
