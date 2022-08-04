7c2454f2a40131dae2a0765cabaa309ae9b74284
abdo-elbeltagy <abdo.elbeltagy401@gmail.com>
2022-08-04 06:47:29 +0200

    node core modules and user defined modules

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+// const os=require('os')
+const path=require('path')
+
 console.log('hello world')
-const {add}=require('./math')
+const {add,sub}=require('./math')  // we can destructure the functions in the module
 console.log(add(2,3))
+console.log(sub(2,3))
 
 // console.log(global)
 
-const os=require('os')
-console.log(os.type())
-console.log(os.version())
-console.log(os.homedir())
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.homedir())
+
+// console.log(__dirname) // for directory names
+// console.log(__filename)  // for file names
+
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
+// console.log(path.parse(__filename))
